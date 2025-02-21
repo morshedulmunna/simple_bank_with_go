2a8d62818bba67fa3bff2e6cecd4b76bf5131acc
morshedulmunna <morshedulmunna1@gmail.com>
2025-02-21 15:50:38 +0600

    cc

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    utils.RandomString(12),
 		Balance:  utils.RandomInt(1, 1000),
@@ -28,5 +28,49 @@ func TestCreateAccount(t *testing.T) {
 	require.NotZero(t, account.CreatedAt)
 	require.NotZero(t, account.UpdatedAt)
 
-	t.Log("CreateAccount test passed")
+	return account
+}
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	deleteAccount, err := testQueries.DeleteAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotNil(t, deleteAccount)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotNil(t, account2)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      account1.ID,
+		Balance: utils.RandomInt(1, 1000),
+	}
+
+	err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+
+}
+
+func TestListAccount(t *testing.T) {
+	createRandomAccount(t)
+	arg := ListAccountsParams{
+		Limit:  1,
+		Offset: 0,
+	}
+	accounts, err := testQueries.ListAccounts(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, accounts)
+	require.Len(t, accounts, 1)
 }
